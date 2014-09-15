930abd12f5d666af99d87ef523a9f7dd59642260
Devin Gaffney <dgaffney@Devin-Gaffneys-MacBook-Pro.local>
2014-09-15 17:12:21 -0400

    updates

--- workers/edge_creator.rb
+++ workers/edge_creator.rb
@@ -6,11 +6,11 @@ class EdgeCreator
   
   def mention_grapher(raw_data)
     raw_data.content.content.extract_screen_names.each do |name|
-      Edge.first_or_create(uuid: mention_grapher_uuid(raw_data)).update_attributes(source: raw_data.screen_name, target: screen_name, attributes: [{for: "twitter_id", value: raw_data.twitter_id}, {for: "created_at", value: raw_data.published}])
+      Edge.first_or_create(uuid: mention_grapher_uuid(raw_data, name)).update_attributes(source: raw_data.screen_name, target: name, attributes: [{for: "twitter_id", value: raw_data.twitter_id}, {for: "created_at", value: raw_data.published}])
     end
   end
   
-  def mention_grapher_uuid(raw_data)
+  def mention_grapher_uuid(raw_data, screen_name)
     [raw_data.screen_name, screen_name, raw_data.twitter_id].join("-")
   end
 end
