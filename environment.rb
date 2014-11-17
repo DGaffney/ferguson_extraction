2fbad571536f01b6c0af831aa6d8cedef9dd7ee9
Devin Gaffney <itsme@devingaffney.com>
2014-11-17 11:17:57 -0500

    updates

--- environment.rb
+++ environment.rb
@@ -13,7 +13,7 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/lib/graph/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/workers/*.rb'].each {|file| require file }
-MongoMapper.connection = Mongo::MongoClient.new("192.168.1.141", :pool_size => 25, :pool_timeout => 60)
+MongoMapper.connection = Mongo::MongoClient.new("24.61.9.153", :pool_size => 25, :pool_timeout => 60)
 MongoMapper.database = "ferguson"
 
 Sidekiq.configure_client do |config|
